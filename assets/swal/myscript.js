4682f4979fab56be1cd3d763750fddce3cd669cd
ivonnanda <ivonnandaantika10935@gmail.com>
2026-04-06 08:06:39 +0700

    perbaikan bug message logout dan pw salah

--- assets/swal/myscript.js
+++ assets/swal/myscript.js
@@ -1,8 +1,30 @@
 const flashData = $('.flash-data').data('flashdata');
-if (flashData) {
+
+if (flashData === 'login_error') {
 	Swal.fire({
-		title: flashData + ' Sukses',
-		text: '',
-		type: 'success'
+		title: 'Login Gagal',
+		text: 'Username atau password salah',
+		icon: 'error'
+	});
+}
+
+if (flashData === 'user_tidak_aktif') {
+	Swal.fire({
+		title: 'User Tidak Aktif',
+		icon: 'warning'
+	});
+}
+
+if (flashData === 'user_tidak_ditemukan') {
+	Swal.fire({
+		title: 'User Tidak Ditemukan',
+		icon: 'error'
+	});
+}
+
+if (flashData === 'Logout') {
+	Swal.fire({
+		title: 'Logout Sukses',
+		icon: 'success'
 	});
 }
